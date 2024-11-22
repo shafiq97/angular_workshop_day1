5b1642cd2647c9625eaf7e440b0f0e25caa4d207
eczhh <hangbicheah12@gmail.com>
2024-11-22 11:58:07 +0800

    add custom directive

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "../header/header.component";
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  isSuccess: boolean = false; // Toggle this to see class changes
+  isVisible: boolean = false;
+  items: string[] = ['Angular', 'React', 'Vue'];
 }
